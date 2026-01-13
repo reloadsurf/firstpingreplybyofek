265cd8bee82791cb1eee6af5a643cea0cca9ae36
7/12/25 version <User@DESKTOP-DQU8F2C>
2026-01-13 13:55:49 +0200

    Add project files.

---
+++ PingReply/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace pr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 255; i++)
+            {
+                String ip = "10.67.200." + i;
+                SendPing(ip);
+            }
+
+        }
+
+        public static void SendPing(String ip)
+        {
+            Ping ping = new Ping();
+            PingReply pingreply = ping.Send(ip);
+            Console.WriteLine($"{ip} status is: {pingreply.Status.ToString()}");
+
+        }
+    }
+}
